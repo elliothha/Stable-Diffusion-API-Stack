50800fc486f3183c585ff135f9cce984633cdf7d
elliothha <elliot.ha@duke.edu>
2023-05-30 10:11:58 -0700

    Testing Commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*'],
+  theme: {
+    extend: {},
+    letterSpacing: {
+      widest: '.20em',
+    },
+    fontFamily: {
+      title: ['Montserrat'],
+    },
+  },
+  plugins: [],
+}
+
